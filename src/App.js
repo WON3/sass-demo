cf465561f67a73cff1054d267e3eb979c3b8cc48
Daniel Valentine <mrdanvalentine@gmail.com>
2018-10-24 21:35:39 -0400

    finished paper bag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-container">
+        <p>Outer</p>
+        <div className="App-outer">
+          <p>inner</p>
+          <div className="App-inner">
+            <div className="App-first"></div>
+            <div className="App-second">
+            </div>
+          </div>
+        </div>
+        <div className="App-outer2"></div>
       </div>
     );
   }
